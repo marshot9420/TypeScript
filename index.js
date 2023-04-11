b70fceccb898cef75c092d2d3c438b9f93c52e65
marshot <124557537+marshot9420@users.noreply.github.com>
2023-04-11 16:20:41 +0900

    타입 미리 정하기 애매할 때

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
-var username = "marshot";
-var age = 28;
-var handsome = true;
-var nothing = null;
-var notYet = undefined;
-var list = [];
-var users = ["marshot", "choi", "yuri"];
-var ages = [1, 2, 3, 4, 5];
-var obj = {};
-var dontKnow = { name: "marshot" };
-var player = 123;
-player = "marshot";
-var hero = 123;
-hero = "marshot";
-function myFunc(x) {
-    return x * 2;
-}
-var john = [1, true];
-var jackson = { callsign: "marshot" };
-var User = /** @class */ (function () {
-    function User(name) {
-        this.name = name;
-    }
-    return User;
-}());
+// Union Type
+var member = 123;
+member = "marshot";
+var members = [1, "2", 3];
+var myObj = { a: 2 };
+// any Type
+var player;
+player = 123;
+player = [];
+// unknown type
+var username;
+username = 123;
+username = {};
+// 아래는 안됨, any보다 안정적
+var myVar = username;
--- index.ts
+++ index.ts
@@ -1,36 +1,20 @@
-let username: string = "marshot";
-let age: number = 28;
-let handsome: boolean = true;
-let nothing: null = null;
-let notYet: undefined = undefined;
-let list: [] = [];
-let users: string[] = ["marshot", "choi", "yuri"];
-let ages: number[] = [1, 2, 3, 4, 5];
-let obj: {} = {};
-let dontKnow: { name?: string } = { name: "marshot" };
-let player: string | number = 123;
-player = "marshot";
+// Union Type
+let member: number | string = 123;
+member = "marshot";
 
-type MyType = string | number;
+let members: (number | string)[] = [1, "2", 3];
 
-let hero: MyType = 123;
-hero = "marshot";
+let myObj: { a: string | number } = { a: 2 };
 
-function myFunc(x: number): number {
-  return x * 2;
-}
+// any Type
+let player: any;
+player = 123;
+player = [];
 
-type Member = [number, boolean];
-let john: Member = [1, true];
+// unknown type
+let username: unknown;
+username = 123;
+username = {};
 
-type Team = {
-  [callsign: string]: string;
-};
-let jackson: Team = { callsign: "marshot" };
-
-class User {
-  name: string;
-  constructor(name: string) {
-    this.name = name;
-  }
-}
+// 아래는 안됨, any보다 안정적
+let myVar: string = username;
