81f4d7235856e07d871deb8538ab6e50ee850d2b
marshot <124557537+marshot9420@users.noreply.github.com>
2023-04-12 20:55:38 +0900

    함수와 methods에 type alias 지정하는 법

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
-// Literal Types
-var username; // username에는 "marshot"만 들어올 수 있음
-// 타입을 더욱 엄격하게 검사
-var introduce; // 유니온 타입도 가능
-function myFunc(a) {
-    // 이렇게하면 파라미터에 hello만 가능
-}
-function myFunc2(a) {
-    // 이렇게하면 1 또는 0만 return 가능
-    return 1;
-}
-// 숙제
-function rockPaperScissors(hand) {
-    return ["rock"];
-}
-var player = {
-    name: "marshot",
+// 함수 type alias 하려면 함수표현식(선언식x) 사용
+var myFunc = function () {
+    return 10;
 };
-function playerFunc(name) { }
-playerFunc(player.name); // playerFunc의 파라미터에는 "marshot"이라는 '타입'('자료'가 아닌)이 들어와야 한다
-// 현재 들어온 'player.name'은 type이 "marshot"이 아닌, "marshot"이라는 string이다
-// 해결책
-var playerObj = {
+var userInfo = {
     name: "marshot",
-}; // 이 Object는 literal type 지정 알아서 해줘
-playerFunc(playerObj.name);
-// as const
-// - object value를 그대로 타입으로 지정해 준다
-// - object 속성들에 모두 readonly 붙여준다
+    plusOne: function () {
+        return 1;
+    },
+};
--- index.ts
+++ index.ts
@@ -1,41 +1,19 @@
-// Literal Types
-let username: "marshot"; // username에는 "marshot"만 들어올 수 있음
-// 타입을 더욱 엄격하게 검사
-let introduce: "marshot" | "genius"; // 유니온 타입도 가능
+// type alia에 함수 type 저장해서 쓰는 법
+type MyFuncType = (a: string) => number;
 
-function myFunc(a: "hello") {
-  // 이렇게하면 파라미터에 hello만 가능
-}
-
-function myFunc2(a: "hello"): 1 | 0 {
-  // 이렇게하면 1 또는 0만 return 가능
-  return 1;
-}
-
-// 숙제
-function rockPaperScissors(
-  hand: "rock" | "paper" | "scissors"
-): ("rock" | "paper" | "scissors")[] {
-  return ["rock"];
-}
-
-var player = {
-  name: "marshot",
+// 함수 type alias 하려면 함수표현식(선언식x) 사용
+const myFunc: MyFuncType = () => {
+  return 10;
 };
 
-function playerFunc(name: "marshot") {}
+// Object 내부의 함수에 타입 지정하는 방법
+type UserInfoType = {
+  plusOne: MyFuncType;
+};
 
-playerFunc(player.name); // playerFunc의 파라미터에는 "marshot"이라는 '타입'('자료'가 아닌)이 들어와야 한다
-// 현재 들어온 'player.name'은 type이 "marshot"이 아닌, "marshot"이라는 string이다
-
-// 해결책
-
-var playerObj = {
+let userInfo = {
   name: "marshot",
-} as const; // 이 Object는 literal type 지정 알아서 해줘
-
-playerFunc(playerObj.name);
-
-// as const
-// - object value를 그대로 타입으로 지정해 준다
-// - object 속성들에 모두 readonly 붙여준다
+  plusOne: () => {
+    return 1;
+  },
+};
