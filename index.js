528ec556ba37f41aa16c391fb210213ecb1989de
marshot <124557537+marshot9420@users.noreply.github.com>
2023-04-14 17:03:32 +0900

    글로벌 모듈화 하는 법

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var age = 28;
 // ts파일에 타입 정의가 너무 길면 d.ts파일 만들기도 함
 // import/export 할게 많으면 namespace 또는 import * as 사용
+var player = "marshot"; // ./types/common/main.d.ts 의 타입
+// npm 설치 시 대부분 type도 들어옴
--- index.ts
+++ index.ts
@@ -5,3 +5,7 @@ let age: Age = 28;
 
 // ts파일에 타입 정의가 너무 길면 d.ts파일 만들기도 함
 // import/export 할게 많으면 namespace 또는 import * as 사용
+
+let player: User = "marshot"; // ./types/common/main.d.ts 의 타입
+
+// npm 설치 시 대부분 type도 들어옴
