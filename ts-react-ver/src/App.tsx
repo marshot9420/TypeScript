46733814836b39a2993bc1c954e0994919a718ee
marshot <124557537+marshot9420@users.noreply.github.com>
2023-04-14 16:18:14 +0900

    useState 타입지정

--- ts-react-ver/src/App.tsx
+++ ts-react-ver/src/App.tsx
@@ -1,9 +1,16 @@
+import React, { useState } from "react";
+
 // React + TS 쓰는 법: 변수, 함수만들 때 타입 지정
 let username: string = "marshot";
 
 let box: JSX.Element = <div></div>; // JSX 타입 지정
 
 const App = () => {
+  // useState는 자동으로 타입이 지정된다.
+  let [user, setUser] = useState(["marshot"]);
+  // 다만 (매우 드물지만) string + number 타입을 지정하고싶다면
+  let [player, setPlayer] = useState<string | number>("marshot");
+
   return (
     <div>
       <h1>Hello</h1>
